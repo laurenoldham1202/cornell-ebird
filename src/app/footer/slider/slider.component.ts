46b6e3f1b9ba123629f03ce3f8c42569738f09b3
Lauren Oldham <lauren.oldham@uky.edu>
2020-11-15 14:11:49 -0500

    Clean code

--- src/app/footer/slider/slider.component.ts
+++ src/app/footer/slider/slider.component.ts
@@ -31,43 +31,28 @@ export class SliderComponent implements OnInit {
 
   onSliderChange(week) {
     this.updateWeek(week.value);
-    // this.week = week.value;
-    // this.sliderService.updateWeek(week.value);
   }
 
-  // var refreshId = setInterval(function() {
-  //   var properID = CheckReload();
-  //   if (properID > 0) {
-  //     clearInterval(refreshId);
-  //   }
-  // }, 10000);
-
+  // TODO add option to not autoplay
   playAnimation() {
+    // toggle button between play and pause
     this.activePlay = !this.activePlay;
 
-    // if (this.activePlay) {
-      // TODO add option to not autoplay
-      // begin play count at current slider position
-      // let count = this.week;
-
-      const animation = setInterval(() => {
-
-
-        if (this.activePlay) {
-          // reset to week 1 automatically
-          if (this.week > 51) {
-            this.week = 1;
-          }
-          // add to current week count
-          this.week++;
-
-          // update app-wide
-          this.updateWeek(this.week);
-        } else {
-          clearInterval(animation);
+    const animation = setInterval(() => {
+      if (this.activePlay) {
+        // reset to week 1 automatically when final week is reached
+        if (this.week > 51) {
+          this.week = 1;
         }
-      }, 250);
+        // add to current week count
+        this.week++;
 
+        // update app-wide
+        this.updateWeek(this.week);
+      } else {
+        // clear interval when slider is paused
+        clearInterval(animation);
+      }
+    }, 250);
   }
-
 }
