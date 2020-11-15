1922496f1d9beab3ee06d413cebc35293d150660
Lauren Oldham <lauren.oldham@uky.edu>
2020-11-15 11:16:01 -0500

    Update color scheme

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -28,7 +28,7 @@ export class MapComponent implements OnInit {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
       center: { lon: -86.76249, lat: 45.10906 },
-      zoom: 5.60,
+      zoom: 4.60,
     });
 
     this.map.on('style.load', () => {
@@ -37,35 +37,36 @@ export class MapComponent implements OnInit {
         data: this.data,
       });
 
+      // ['#fec027', '#f1a43a', '#e4874e', '#d76862', '#c7457a', '#a43580', '#7e2884', '#561a88', '#1e078d']
       this.map.addLayer({
         'id': 'birds',
         'type': 'fill',
         'source': 'birds',
         'paint': {
-          'fill-opacity': 0.5,
+          'fill-opacity': 0.95,
           'fill-outline-color': '#EEE',
           'fill-color': [
             'interpolate',
             ['linear'],
             ['get', 'stats_week_1'],  // default at 1 week
             0,
-            '#F2F12D',
-            0.075,
-            '#EED322',
+            '#fec027',
             0.15,
-            '#E6B71E',
-            0.2,
-            '#DA9C20',
-            0.33,
-            '#CA8323',
-            1,
-            '#B86B25',
-            3,
-            '#A25626',
-            4.5,
-            '#8B4225',
-            6,
-            '#723122'
+            '#f1a43a',
+            0.43,
+            '#e4874e',
+            0.66,
+            '#d76862',
+            0.89,
+            '#c7457a',
+            1.18,
+            '#a43580',
+            1.61,
+            '#7e2884',
+            2.09,
+            '#561a88',
+            3.4,
+            '#1e078d'
           ],
         }
       });
@@ -75,30 +76,31 @@ export class MapComponent implements OnInit {
       this.sliderService.week$.subscribe((week: number) => {
         // console.log(this.data.features[10]);
         console.log(this.data.features[10].properties[`stats_week_${week}`]);
+        // 0 - 3.4
         this.map.setPaintProperty('birds', 'fill-color',
           [
             'interpolate',
             ['linear'],
             ['get', `stats_week_${week}`],
             0,
-            '#F2F12D',
-            0.075,
-            '#EED322',
+            '#fec027',
             0.15,
-            '#E6B71E',
-            0.2,
-            '#DA9C20',
-            0.33,
-            '#CA8323',
-            1,
-            '#B86B25',
-            3,
-            '#A25626',
-            4.5,
-            '#8B4225',
-            6,
-            '#723122'
-          ],)
+            '#f1a43a',
+            0.43,
+            '#e4874e',
+            0.66,
+            '#d76862',
+            0.89,
+            '#c7457a',
+            1.18,
+            '#a43580',
+            1.61,
+            '#7e2884',
+            2.09,
+            '#561a88',
+            3.4,
+            '#1e078d'
+          ])
       });
     });
 
