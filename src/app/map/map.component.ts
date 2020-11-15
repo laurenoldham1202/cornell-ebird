4fe49ccb57834fc382faa9c8a29c3d6ea4f2c236
Lauren Oldham <lauren.oldham@uky.edu>
2020-11-15 11:00:05 -0500

    Dynamically update hexbins with slider

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -31,16 +31,78 @@ export class MapComponent implements OnInit {
       zoom: 5.60,
     });
 
-    // console.log(this.data);
+    this.map.on('style.load', () => {
+      this.map.addSource('birds', {
+        type: 'geojson',
+        data: this.data,
+      });
 
-    this.data.features.forEach(feature => {
-      // console.log(feature);
+      this.map.addLayer({
+        'id': 'birds',
+        'type': 'fill',
+        'source': 'birds',
+        'paint': {
+          'fill-opacity': 0.5,
+          'fill-outline-color': '#EEE',
+          'fill-color': [
+            'interpolate',
+            ['linear'],
+            ['get', 'stats_week_1'],  // default at 1 week
+            0,
+            '#F2F12D',
+            0.075,
+            '#EED322',
+            0.15,
+            '#E6B71E',
+            0.2,
+            '#DA9C20',
+            0.33,
+            '#CA8323',
+            1,
+            '#B86B25',
+            3,
+            '#A25626',
+            4.5,
+            '#8B4225',
+            6,
+            '#723122'
+          ],
+        }
+      });
+
+      console.log(this.map.getSource('birds'));
+
+      this.sliderService.week$.subscribe((week: number) => {
+        // console.log(this.data.features[10]);
+        console.log(this.data.features[10].properties[`stats_week_${week}`]);
+        this.map.setPaintProperty('birds', 'fill-color',
+          [
+            'interpolate',
+            ['linear'],
+            ['get', `stats_week_${week}`],
+            0,
+            '#F2F12D',
+            0.075,
+            '#EED322',
+            0.15,
+            '#E6B71E',
+            0.2,
+            '#DA9C20',
+            0.33,
+            '#CA8323',
+            1,
+            '#B86B25',
+            3,
+            '#A25626',
+            4.5,
+            '#8B4225',
+            6,
+            '#723122'
+          ],)
+      });
     });
 
-    this.sliderService.week$.subscribe((week: number) => {
-      // console.log(this.data.features[10]);
-      console.log(this.data.features[10].properties[`stats_week_${week}`]);
-    });
+
   }
 
 }
